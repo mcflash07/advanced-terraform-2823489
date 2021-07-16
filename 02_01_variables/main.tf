8304f29307d27994d3feb899c8bcdd1d3a5e1422
mcflash07 <mcflash07.ae@gmail.com>
2021-07-16 10:34:06 +0100

    Use default profile and default region eu-west-1

--- 02_01_variables/main.tf
+++ 02_01_variables/main.tf
@@ -1,12 +1,8 @@
 # //////////////////////////////
 # VARIABLES
 # //////////////////////////////
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
 variable "region" {
-  default = "us-east-2"
+  default = "eu-west-1"
 }
 
 variable "vpc_cidr" {
@@ -68,8 +64,7 @@ variable "environment_instance_settings" {
 # PROVIDERS
 # //////////////////////////////
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  profile = "default"
   region     = var.region
 }
 
